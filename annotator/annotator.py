338d236b77cf35bb820e6b6a9a40b205d511e586
Fangfang Xia <fangfang.xia@gmail.com>
2017-07-17 14:24:08 -0500

    extract latent features

--- annotator/annotator.py
+++ annotator/annotator.py
@@ -14,14 +14,12 @@ from keras.callbacks import Callback, ModelCheckpoint, ReduceLROnPlateau, Tensor
 from keras.layers import Activation, Conv1D, Dense, GlobalMaxPooling1D
 from keras.models import Sequential
 from sklearn.model_selection import train_test_split
+from utils import load_data_coreseed, load_data_100, load_data_1K
 
 
 logger = logging.getLogger(__name__)
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
-
-CHARS = ' atgc'
-CHARLEN = len(CHARS)
 
 # MAXLEN = 3000
 MAXLEN = 60 * 60
@@ -41,50 +39,6 @@ DATA = '1K'
 MODEL = 'res'
 MODEL_VARIATION = 'v1'
 SAVE = 'save/save'
-
-
-class CharacterTable(object):
-    '''
-    Given a set of characters:
-    + Encode them to a one hot integer representation
-    + Decode the one hot integer representation to their character output
-    + Decode a vector of probabilities to their character output
-    '''
-    def __init__(self, chars, maxlen):
-        self.chars = sorted(set(chars))
-        self.char_indices = dict((c, i) for i, c in enumerate(self.chars))
-        self.indices_char = dict((i, c) for i, c in enumerate(self.chars))
-        self.maxlen = maxlen
-
-    def encode(self, C, maxlen=None, snake2d=False):
-        maxlen = maxlen if maxlen else self.maxlen
-        X = np.zeros((maxlen, len(self.chars)))
-        for i, c in enumerate(C):
-            X[i, self.char_indices[c]] = 1
-        if snake2d:
-            a = int(np.sqrt(maxlen))
-            X2 = np.zeros((a, a, len(self.chars)))
-            for i in range(a):
-                for j in range(a):
-                    k = i * a
-                    k += a - j - 1 if i % 2 else j
-                    X2[i, j] = X[k]
-            X = X2
-        return X
-
-    def decode(self, X, snake2d=False):
-        X = X.argmax(axis=-1)
-        if snake2d:
-            a = X.shape[0]
-            X2 = np.zeros(a * a)
-            for i in range(a):
-                for j in range(a):
-                    k = i * a
-                    k += a - j - 1 if i % 2 else j
-                    X2[k] = X[i, j]
-            X = X2
-        C = ''.join(self.indices_char[x] for x in X)
-        return C
 
 
 def get_parser():
@@ -138,92 +92,6 @@ def get_parser():
                         help='data')
 
     return parser
-
-
-def load_data_100(maxlen=1000, val_split=0.2, batch_size=128, snake2d=False):
-    ctable = CharacterTable(CHARS, maxlen)
-
-    df = pd.read_csv('ref.100ec.pgf.seqs.filter', sep='\t', engine='c', dtype={'genome': str})
-
-    n = df.shape[0]
-    if snake2d:
-        a = int(np.sqrt(maxlen))
-        x = np.zeros((n, a, a, CHARLEN), dtype=np.byte)
-    else:
-        x = np.zeros((n, maxlen, CHARLEN), dtype=np.byte)
-
-    for i, seq in enumerate(df['feature.na_sequence']):
-        x[i] = ctable.encode(seq[:maxlen], snake2d=snake2d)
-
-    y = pd.get_dummies(df.iloc[:, 0]).values
-    classes = df.iloc[:, 0].nunique()
-
-    # df_ref = pd.read_csv('ref.patric_ids', header=None, dtype=str)
-    # mask = df['genome'].isin(df_ref[0].sample(100))
-    # x_train = x[mask]
-    # y_train = y[mask]
-    # x_val = x[~mask]
-    # y_val = y[~mask]
-
-    x_train, x_val, y_train, y_val = train_test_split(x, y, test_size=0.2,
-                                                      random_state=SEED,
-                                                      stratify=df.iloc[:, 0])
-
-    return (x_train, y_train), (x_val, y_val), classes
-
-
-def load_data_1K(maxlen=1000, val_split=0.2, batch_size=128, snake2d=False):
-    ctable = CharacterTable(CHARS, maxlen)
-
-    df = pd.read_csv('rep.1000ec.pgf.seqs.filter', sep='\t', engine='c', dtype={'genome': str})
-    # df = pd.read_csv('rep.1000ec.pgf.seqs.filter', nrows=10000, sep='\t', engine='c', dtype={'genome':str})
-
-    n = df.shape[0]
-    if snake2d:
-        a = int(np.sqrt(maxlen))
-        x = np.zeros((n, a, a, CHARLEN), dtype=np.byte)
-    else:
-        x = np.zeros((n, maxlen, CHARLEN), dtype=np.byte)
-
-    for i, seq in enumerate(df['feature.na_sequence']):
-        x[i] = ctable.encode(seq[:maxlen].lower(), snake2d=snake2d)
-
-    y = pd.get_dummies(df.iloc[:, 0]).values
-    classes = df.iloc[:, 0].nunique()
-
-    x_train, x_val, y_train, y_val = train_test_split(x, y, test_size=0.2,
-                                                      random_state=SEED,
-                                                      stratify=df.iloc[:, 0])
-    return (x_train, y_train), (x_val, y_val), classes
-
-
-# def reshape_snake_2d(x1d, axis=1):
-    # side = int(np.sqrt(x1d))
-
-
-def load_data_coreseed(maxlen=1000, val_split=0.2, batch_size=128, snake2d=False):
-    ctable = CharacterTable(CHARS, maxlen)
-
-    df = pd.read_csv('coreseed.train.tsv', sep='\t', engine='c',
-                     usecols=['function_index', 'dna'])
-
-    n = df.shape[0]
-    if snake2d:
-        a = int(np.sqrt(maxlen))
-        x = np.zeros((n, a, a, CHARLEN), dtype=np.byte)
-    else:
-        x = np.zeros((n, maxlen, CHARLEN), dtype=np.byte)
-
-    for i, seq in enumerate(df['dna']):
-        x[i] = ctable.encode(seq[:maxlen].lower(), snake2d=snake2d)
-
-    y = pd.get_dummies(df.iloc[:, 0]).values
-    classes = df.iloc[:, 0].nunique()
-
-    x_train, x_val, y_train, y_val = train_test_split(x, y, test_size=0.2,
-                                                      random_state=SEED,
-                                                      stratify=df.iloc[:, 0])
-    return (x_train, y_train), (x_val, y_val), classes
 
 
 def extension_from_parameters(args):
---
+++ annotator/extract_latent.py
@@ -0,0 +1,66 @@
+from __future__ import print_function
+
+import numpy as np
+import pandas as pd
+
+import keras
+
+from res50_nt import Res50NT
+from utils import CharacterTable
+from utils import load_data_coreseed
+
+CHARS = ' atgc'
+CHARLEN = len(CHARS)
+MAXLEN = 3600
+
+
+base_model = Res50NT(input_shape=(MAXLEN, CHARLEN),
+                     dense_layers=[1000],
+                     dropout=0.1,
+                     activation='selu',
+                     variation='v1',
+                     classes=1000)
+
+weights_fname = 'save.res.DATA=core.A=selu.B=100.E=100.M=resv1.O=sgd.LEN=3600.R=0.01.D=0.1.D1=1000.h5'
+weights_url = 'file:///home/fangfang/big/deepbio/annotator/save/' + weights_fname
+weights_path = keras.utils.data_utils.get_file(weights_fname, weights_url,
+                                               cache_dir='.',
+                                               cache_subdir='weights')
+
+base_model.load_weights(weights_path)
+model = keras.models.Model(inputs=base_model.input, outputs=base_model.get_layer('fc1').output)
+
+# We can load core seed data with fixed train/validation partitions using the default random seed (2017)
+#
+# (x_train, y_train), (x_val, y_val), classes = load_data_coreseed(maxlen=args.maxlen, snake2d=snake2d)
+#
+# x_val_latent = base_model.predict(x, verbose=1)
+
+# Or we can load the sequences with
+df_func = pd.read_csv('func.top1000', sep='\t', names=['count', 'function'], engine='c')
+df_func['function_index'] = range(1, len(df_func) + 1)
+func_dict = df_func.set_index('function_index')['function'].to_dict()
+
+df = pd.read_csv('coreseed.train.tsv', sep='\t', engine='c', nrows=10,
+                 usecols=['peg', 'function_index', 'dna'])
+
+ctable = CharacterTable(CHARS, MAXLEN)
+n = df.shape[0]
+x = np.zeros((n, MAXLEN, CHARLEN), dtype=np.byte)
+for i, seq in enumerate(df['dna']):
+    x[i] = ctable.encode(seq[:MAXLEN].lower())
+    # print(i, seq)
+
+y_proba = base_model.predict(x, verbose=1)
+y_conf = y_proba.max(axis=-1)
+y_pred = y_proba.argmax(axis=-1) + 1
+y_true = df['function_index']
+
+x_latent = model.predict(x, verbose=1)
+
+for i in range(n):
+    print('X[{}]: {}'.format(i, x[i]))
+    print('Latent features:', x_latent[i])
+    print('y_pred: {}, {}', y_pred[i], func_dict[y_pred[i]])
+    print('y_true: {}, {}', y_true[i], func_dict[y_true[i]])
+    print('\n')
